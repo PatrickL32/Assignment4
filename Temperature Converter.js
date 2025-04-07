19d3d70d6d88f26cd08e2866c02bae6a7aa052a0
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-04-07 15:36:56 -0700

    first commit

--- Temperature Converter.js
+++ Temperature Converter.js
@@ -1,14 +1,27 @@
-function convertTemperatureRange(startValue,endValue, scale) {
-    for (let temp=startValue; temp<=endValue;temp++) {
-        if (scale==="C") {
-            const fahnrenheit = (temp * 9/5) +32;//convert celsius to Fahrenheit
-            console.log(`${temp}°C=${fahnrenheit.toFixed(2)}°F`);
-        } else if (scale==="F") {
-            const celsius = (temp-32) *5/9;//convert Fahrenheit to celsius
-            console.log(`${temp}° F = ${ celsius.toFixed(2)}°C`);
-        }else{
-            console.log("Invalid scale. Please use 'C' for celsius of 'F' for Fahrenheit.");
+function convertTemperatureRange() {
+    let startValue=document.getElementById("startValue").value;
+    let endValue=document.getElementById("endValue").value;
+    let scale=document.getElementById("scale").value;
+
+    for (let temp = startValue; temp <= endValue; temp++) {//loop from 0 to 5
+        if (scale === "C") {
+            const fahrenheit = (temp * 9 / 5) + 32; // Convert Celsius to Fahrenheit
+            console.log(`${temp}°C = ${fahrenheit.toFixed(2)}°F`);
+            document.getElementById("output").innerHTML += `<li> ${temp}°C = ${fahrenheit.toFixed(2)}°F <li>`;
+        } else if (scale === "F") {
+            const celsius = (temp - 32) * 5 / 9; // Convert Fahrenheit to Celsius
+            console.log(`${temp}°F = ${celsius.toFixed(2)}°C`);
+            document.getElementById("output").innerHTML += `<li> ${temp}°F = ${celsius.toFixed(2)}°C <li>`
+        } else {
+            console.log("Invalid scale. Please use 'C' for Celsius or 'F' for Fahrenheit.");
+            document.getElementById("output").innerHTML += `<li> Invalid scale. Please use 'C' for Celsius or 'F' for Fahrenheit. </li>`
             break;
         }
     }
 }
+
+//call the function for celsius to fahrenheit
+//convertTemperatureRange(0, 7, "C"); // Converts 0°C to 5°C to Fahrenheit
+
+//call the function for Fahrenheit to Celsius
+//convertTemperatureRange(32, 36, "F"); // Converts 32°F to 36°F to Celsius
